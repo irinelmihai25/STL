112a4a9ef7c7c4236bb778f46bd4e4673d1798fc
Irinel Mihai <irinelmihai25@gmail.com>
2024-11-11 15:39:22 +0200

    c1

--- main.cpp
+++ main.cpp
@@ -1,32 +1,52 @@
 #include <iostream>
 #include <fstream>
 #include <string>
-
+#include <vector>
 using namespace std;
-
 int main()
 {
-    ifstream inFile("input.txt");
+    ifstream inFile("input3.txt");
 
     int no_problems, no_doctors;
     string name, speciality;
-    
-    inFile >> no_problems;
 
+    inFile >> no_problems;
+    vector<string> numeafectiune;
+    vector<string> numespecialitate;
     for (int i = 0; i < no_problems; i++)
     {
         inFile >> name;
+        numeafectiune.push_back(name);
         inFile >> speciality;
-        cout << name << ' ' << speciality << '\n';
+        numespecialitate.push_back(speciality);
+        //cout << name << ' ' << speciality << '\n';
     }
 
     inFile >> no_doctors;
-
+    vector<string> numespecialitatedoctor;
     for (int i = 0; i < no_doctors; i++)
     {
         inFile >> name;
         inFile >> speciality;
-        cout << name << ' ' << speciality << '\n';
+        numespecialitatedoctor.push_back(speciality);
+        //cout << name << ' ' << speciality << '\n';
+    }
+    int i = 0;
+    for (auto nume = numeafectiune.begin(); nume < numeafectiune.end(); nume++) {
+        int ok = 0;
+        for (auto specialitatedoc = numespecialitatedoctor.begin(); specialitatedoc < numespecialitatedoctor.end(); specialitatedoc++) {
+            if (*specialitatedoc == numespecialitate[i]) {
+                cout << numeafectiune[i] << " Acceptat" << endl;
+                ok = 1;
+                break;
+            }
+
+        }
+        if (ok == 0) {
+            cout << numeafectiune[i] << " Respins" << endl;
+
+        }
+        i++;
     }
 
     return 0;
