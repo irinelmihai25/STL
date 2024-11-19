e1c6038c484421b21fbc6dfa157655a91ec9b557
irinel <irinelmihai25@gmail.com>
2024-11-19 13:35:43 +0200

    c2

--- main.cpp
+++ main.cpp
@@ -3,51 +3,124 @@
 #include <string>
 #include <vector>
 using namespace std;
-int main()
-{
-    ifstream inFile("input3.txt");
 
+struct problem {
+    std::string id;
+    std::string specialitate;
+    int durata;
+    bool rezolvat = false;
+};
+
+struct doctor {
+    std::string id;
+    std::string specialitate;
+    int timpdelucru = 8;
+
+};
+
+int main() {
+    ifstream inFile("input4_bonus.txt");
     int no_problems, no_doctors;
-    string name, speciality;
-
     inFile >> no_problems;
-    vector<string> numeafectiune;
-    vector<string> numespecialitate;
-    for (int i = 0; i < no_problems; i++)
-    {
-        inFile >> name;
-        numeafectiune.push_back(name);
-        inFile >> speciality;
-        numespecialitate.push_back(speciality);
-        //cout << name << ' ' << speciality << '\n';
+    std::vector<problem> probleme(no_problems);
+
+    for (int i = 0; i < no_problems; i++) {
+        inFile >> probleme[i].id;
+        inFile >> probleme[i].specialitate;
+        inFile >> probleme[i].durata;
+    }
+    inFile >> no_doctors;
+    std::vector<doctor> doctori(no_doctors);
+
+    for (int i = 0; i < no_doctors; i++) {
+        inFile >> doctori[i].id;
+        inFile >> doctori[i].specialitate;
+    }
+    for (auto& iter : doctori) {
+
+        std::vector<string> rezolvate;
+        int problemerezolvate = 0;
+        for (auto& iter2 : probleme) {
+            std::vector<string> afecrez;
+            if (!iter2.rezolvat && (iter.specialitate == iter2.specialitate) && (iter2.durata <= iter.timpdelucru)) {
+                iter.timpdelucru = iter.timpdelucru - iter2.durata;
+                iter2.rezolvat = true;
+                problemerezolvate++;
+                rezolvate.push_back(iter2.id);
+
+
+            }
+            if (iter.timpdelucru == 0)
+                break;
+        }
+        cout << iter.id << " " << problemerezolvate << " ";
+        for (int i = 0; i < rezolvate.size(); ++i) {
+            cout << rezolvate[i] << " ";
+
+        }
+        cout << endl;
+
+    }
+    return 0;
+}
+/*problema de dinainte de bonus
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+using namespace std;
+
+struct problem {
+    std::string id;
+    std::string specialitate;
+};
+
+struct doctor {
+    std::string id;
+    std::string specialitate;
+};
+
+int main() {
+    ifstream inFile("input.txt");
+    int no_problems, no_doctors;
+    inFile >> no_problems;
+    std::vector<problem> probleme(no_problems);
+
+    for (int i = 0; i < no_problems; i++) {
+        inFile >> probleme[i].id;
+        inFile >> probleme[i].specialitate;
     }
 
     inFile >> no_doctors;
-    vector<string> numespecialitatedoctor;
-    for (int i = 0; i < no_doctors; i++)
-    {
-        inFile >> name;
-        inFile >> speciality;
-        numespecialitatedoctor.push_back(speciality);
-        //cout << name << ' ' << speciality << '\n';
+    std::vector<doctor> doctori(no_doctors);
+
+    for (int i = 0; i < no_doctors; i++) {
+        inFile >> doctori[i].id;
+        inFile >> doctori[i].specialitate;
     }
-    int i = 0;
-    for (auto nume = numeafectiune.begin(); nume < numeafectiune.end(); nume++) {
-        int ok = 0;
-        for (auto specialitatedoc = numespecialitatedoctor.begin(); specialitatedoc < numespecialitatedoctor.end(); specialitatedoc++) {
-            if (*specialitatedoc == numespecialitate[i]) {
-                cout << numeafectiune[i] << " Acceptat" << endl;
-                ok = 1;
+
+    for (auto problemiter : probleme) {
+        bool accepted = false;
+        
+
+        for (auto it = doctori.begin(); it != doctori.end(); ++it) {
+            if (problemiter.specialitate == it->specialitate) {
+
+                std::cout << it->id << " " << problemiter.id << endl;
+                accepted = true;
+                doctori.erase(it);
                 break;
             }
-
         }
-        if (ok == 0) {
-            cout << numeafectiune[i] << " Respins" << endl;
 
-        }
-        i++;
     }
 
     return 0;
 }
+
+*/
+
+
+
+
+
